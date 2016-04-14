ffeb2055adbb27c4db29e4d94fe8491d56a6a23a
Mark <mmichaelsen678@gmail.com>
2016-04-14 11:41:53 -0300

    query

--- app/querytester.php
+++ app/querytester.php
@@ -33,6 +33,7 @@ if (isguestuser()){
 }
 
 $moodleid = $USER->id;
+
 $course = $DB->get_record('course', array('fullname' => 'Curso de gente'));
 
 echo "Id: ".$course->id."\n Course: ".$course->fullname."\n";
