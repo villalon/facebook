d214327403e383ebd03b434fa4fed5529a1171af
Mark <mmichaelsen678@gmail.com>
2016-04-18 20:39:47 -0300

    No define

--- app/request.php
+++ app/request.php
@@ -21,8 +21,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
  
-define("AJAX_SCRIPT", true);
-define("NO_DEBUG_DISPLAY", true);
+//define("AJAX_SCRIPT", true);
+//define("NO_DEBUG_DISPLAY", true);
 
 require_once (dirname ( dirname ( dirname ( dirname ( __FILE__ ) ) ) ) . '/config.php');
 require_once ($CFG->dirroot . '/local/facebook/locallib.php');
