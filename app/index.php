10dacf4278c4aa0819eefdceb96bb10211166994
jorgecabane93 <jorgecabane93@gmail.com>
2016-05-02 19:02:00 -0300

    javascripts exportados hacia carpeta js

--- app/index.php
+++ app/index.php
@@ -145,13 +145,13 @@ if ($userfacebookinfo != false) {
 		$shortname = $courses->shortname;
 		$totals = $courses->totalnotifications;
 		
-		echo '<div class="block" style="height: 4em;"><button type="button" class="btn btn-info btn-lg" style="white-space: normal; width: 90%; height: 90%; border: 1px solid lightgray; background: linear-gradient(white, gainsboro);" courseid="' . $courseid . '" fullname="' . $fullname . '" component="button">';
+		echo '<div class="block" style="height: 4em;"><button type="button" class="btn btn-info btn-lg" style="white-space: normal; width: 90%; height: 90%; border: 1px solid lightgray; background: #F0F0F0;" courseid="' . $courseid . '" fullname="' . $fullname . '" component="button">';
 		
 		if ($totals > 0) {
-			echo '<p class="name" style="position: relative; height: 3em; overflow: hidden; color: black; font-weight: bold; text-decoration: none; font-size:13px; word-wrap: initial;" courseid="' . $courseid . '" component="button">
+			echo '<p class="name" align="left" style="position: relative; height: 3em; overflow: hidden; color: black; font-weight: bold; text-decoration: none; font-size:13px; word-wrap: initial;" courseid="' . $courseid . '" component="button">
  				' . $fullname . '</p><span class="badge" style="color: white; background-color: red; position: relative; right: -58%; top: -64px; margin-right:9%;" courseid="' . $courseid . '" component="button">' . $totals . '</span></button></div>';
 		} else {
-			echo '<p class="name" style="position: relative; height: 3em; overflow: hidden; color: black; font-weight: bold; text-decoration: none; font-size:13px; word-wrap: initial;" courseid="' . $courseid . '" component="button">
+			echo '<p class="name" align="left" style="position: relative; height: 3em; overflow: hidden; color: black; font-weight: bold; text-decoration: none; font-size:13px; word-wrap: initial;" courseid="' . $courseid . '" component="button">
  				' . $fullname . '</p></button></div>';
 		}
 	}
@@ -161,7 +161,7 @@ if ($userfacebookinfo != false) {
 	// include 'htmltoinclude/news.html';
 	echo "</div>";
 	
-	echo "<div class='col-md-10 col-sm-9 col-xs-12'>";
+	echo "<div class='col-md-9 col-sm-9 col-xs-12'>";
 	echo "<div class='advert'><div style='position: relative;'><img src='images/jpg_an_1.jpg'style='margin-top:10%; margin-left:8%; width:35%'><img src='images/jpg_an_2.jpg' style='margin-top:10%; margin-left:5%; width:35%'></div></div>";
 	echo "<div id='loadinggif' align='center' style='margin-top: 10%; text-align: center; display:none;'><img src='https://webcursos-d.uai.cl/local/facebook/app/images/ajaxloader.gif'></div>";
 	echo "<div id='table-body'></div>";
@@ -181,113 +181,6 @@ if ($userfacebookinfo != false) {
 	?>
 	
 	<!-- Display engine -->
-
-	<script type="text/javascript">
-	$(document).ready(function () {
-		var courseId = null;
-		var discussionId = null;
-		var emarkingId = null;
-		var assignId = null;
-		var moodleId = "<?php echo $moodleid; ?>";
-	
-		$("*", document.body).click(function(event) {
-			event.stopPropagation();
-	
-			var courseid = $(this).parent().parent().attr('courseid');
-			var badgecourseid = $( "button[courseid='"+courseid+"']" ).parent().find('.badge');
-			var aclick = $(this).parent().attr('style');
-			var advert = $(this).parent().parent().parent().parent().parent().find('.advert');
-			
-	
-			if (($(this).attr('component') == "button") && ($(this).attr('courseid') != courseId)) {
-				
-				courseId = $(this).attr('courseid');
-				advert.remove();
-				$('#table-body').empty();
-	
-				// Ajax fix
-				jQuery.ajax({
-					url : "https://webcursos-d.uai.cl/local/facebook/app/request.php?action=get_course_data&moodleid=" + moodleId + "&courseid=" + courseId,
-					async : true,
-					data : {},
-					   beforeSend: function(){
-						   $("#loadinggif").show();
-						   },
-					success : function(response) {
-						$('#table-body').empty();
-						$('#table-body').hide();
-						$('#table-body').append('<div>' + response + '</div>');
-						$('#table-body').fadeIn(300);
-					},
-					   complete: function(){
-						   $("#loadinggif").hide();
-						   }
-				});
-			}		
- 				 			
- 			else if($(this).attr('component') == "close-modal") {		
- 				modalId = $(this).attr('modalid');		
- 				$('#' + modalId).modal('hide');		
- 			}
-	
-			else if($(this).attr('component') == "assign") {
-				assignId = $(this).attr('assignid');
-				$('#a' + assignId).modal('show');
-	
-				if(aclick == 'font-weight:bold'){			
-					 $(this).parent().parent().children("td").css('font-weight','normal');
-	//				 $(this).parent().parent().children("td").children("button").removeClass("btn btn-primary");
-	//				 $(this).parent().parent().children("td").children("button").addClass("btn btn-default");
-					 $(this).parent().parent().children("td").children("center").children("span").css('color','transparent');
-					 $(this).parent().parent().children("td").children("button").css('color','#909090');
-					 				
-					 if(badgecourseid.text() == 1) { 
-					 	badgecourseid.remove(); 
-					 }
-					 else{ 
-					 	badgecourseid.text(badgecourseid.text()-1); 
-					 }
-				}
-			}
-			else if($(this).attr('component') == "other") {
-				
-				if(aclick == 'font-weight:bold'){
-					
-					$(this).parent().parent().children("td").css('font-weight','normal');
-	//				$(this).parent().parent().children("td").children("button").removeClass("btn btn-primary");
-	//				$(this).parent().parent().children("td").children("button").addClass("btn btn-default");
-					$(this).parent().parent().children("td").children("center").children("span").css('color','transparent');
-					$(this).parent().parent().children("td").children("button").css('color','#909090');
-					
-					if(badgecourseid.text() == 1) { 
-						badgecourseid.remove(); 
-					}
-					else{ 
-						badgecourseid.text(badgecourseid.text()-1); 
-					}
-				}
-			}
-		});
-	});
-	</script>
-	<script>
-//script for searching courses
-	$("#search").on('change keyup paste', function() {
-		var searchValue = $('#search').val();
-		$("button").each(function() {
-			var buttonId = $(this).attr('courseid');
-
-			if($(this).attr('fullname').toLowerCase().indexOf(searchValue) == -1) {
-				$(this).hide();
-				$(this).parent().css('height', '0');
-			} else {
-				$(this).show();
-				$(this).parent().css('height', '4em');
-			}
-		});
-	});
-	</script>
-	
 	
 	<?php
 	echo "</div></div>";
@@ -304,3 +197,8 @@ if ($userfacebookinfo != false) {
 	echo '</div>';
 	include 'htmltoinclude/spacer.html';
 }
+
+	//scripts
+	?>
+	<script type="text/javascript" src="js/onclick.js"></script>
+	<script type="text/javascript" src="js/search.js"></script>
