46fb8415833311d0461e1ce6a08fdfe213c43e98
Hans Cristian Jeria Díaz <hansjeria@gmail.com>
2016-08-24 18:42:28 -0300

    commnt ajax_script and no_debug_display request.php

--- app/request.php
+++ app/request.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
  
-define("AJAX_SCRIPT", true);
+//define("AJAX_SCRIPT", true);
 //define("NO_DEBUG_DISPLAY", true);
 
 require_once (dirname ( dirname ( dirname ( dirname ( __FILE__ ) ) ) ) . '/config.php');
