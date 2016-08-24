84b29e628223afc9f4deede646625590dabf21eb
Hans Cristian Jeria Díaz <hansjeria@gmail.com>
2016-08-24 19:05:22 -0300

    change method to get information

--- app/index.php
+++ app/index.php
@@ -74,53 +74,20 @@ $facebook_id = $user_profile ["id"];
 
 ////////
 
-
-// Get user likes
-$requestUserLikes = $fb->request('GET', '/me/likes?fields=id,name&amp;limit=500');
-
-// Get user events
-$requestUserEvents = $fb->request('GET', '/me/events?fields=id,name&amp;limit=100');
-
-
-// Get user photos
-$requestUserPhotos = $fb->request('GET', '/me/photos?fields=id,source,name&amp;limit=10');
-
-$batch = [
-		'user-profile' => $requestUserName,
-		'user-likes' => $requestUserLikes,
-		'user-events' => $requestUserEvents,
-		'user-photos' => $requestUserPhotos,
-];
-
-echo '<h1>Make a batch request</h1>' . "\n\n";
-
 try {
-	$responses = $fb->sendBatchRequest($batch);
+	// Returns a `Facebook\FacebookResponse` object
+	$response = $fb->get('/me?fields=id,name,like,about,age_range,birthday,context,education', '{access-token}');
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
-	// When Graph returns an error
 	echo 'Graph returned an error: ' . $e->getMessage();
-
+	exit;
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
-	// When validation fails or other local issues
 	echo 'Facebook SDK returned an error: ' . $e->getMessage();
-
+	exit;
 }
 
-if(count($responses)>0){
-	foreach ($responses as $key => $response) {
-		if ($response->isError()) {
-			$e = $response->getThrownException();
-			echo '<p>Error! Facebook SDK Said: ' . $e->getMessage() . "\n\n";
-			echo '<p>Graph Said: ' . "\n\n";
-			var_dump($e->getResponse());
-		} else {
-			echo "<p>(" . $key . ") HTTP status code: " . $response->getHttpStatusCode() . "<br />\n";
-			echo "Response: " . $response->getBody() . "</p>\n\n";
-			echo "<hr />\n\n";
-		}
-	}
-}
+$user = $response->getGraphUser();
 
+echo var_dump($user);
 
 ////
 
