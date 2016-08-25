c016b1df73fd7f4ef98f536556238039b2b0f7e0
Hans Cristian Jeria Díaz <hansjeria@gmail.com>
2016-08-25 16:43:53 -0300

    creating json for save data

--- app/index.php
+++ app/index.php
@@ -92,32 +92,7 @@ try {
 if($getInfo){
 	echo "give me the info";
 	$user = $response->getGraphUser();
-	/*
-	$name = $user['name'];
-	$age_range = $user['age_range'];
-	$birthday = $user['birthday'];
-	$education = $user['education'];
-	$languages = $user['languages'];
-	$location = $user['location'];
-	$political = $user['political'];
-	$devices = $user['devices'];
-	$email = $user['email'];
-	$gender = $user['gender'];
-	$hometown = $user['hometown'];
-	$relationship_status = $user['relationship_status'];
-	$sports = $user['$sports'];
-	$admined_groups = $user['admined_groups'];
-	$events = $user['events'];
-	$admined_groups = $user['admined_groups'];
-	$friends = $user['friends'];
-	$movies = $user['movies'];
-	$tagged_places = $user['tagged_places'];
-	$music = $user['music'];
-	$likes = $user['likes'];
 	
-	print_r($name);
-	print_r($age_range);
-	print_r($likes);
 	/*
 	$json = json_encode($array);
 	$data->information = $json;
@@ -125,18 +100,26 @@ if($getInfo){
 	$DB->update_record('facebook_user', $data);
 	*/
 
+	$jsoninfo = array();
 	foreach ($user as $key=> $value){
-		
 		echo "<br> KEY: ".$key."<br>";
+		
 		if( count($value) == 1 ){			
 			echo " VALUE ".var_dump($value)."<br>";
+			$jsoninfo[$key] = array($value);
 		}else{
+			$data = array();
 			foreach ($value as $valor){
 				echo " VALUE ".$valor."<br>";
+				$data [] = $valor;
 			}
+			$jsoninfo[$key] = $data;
 		}
 		
 	}
+	print_r($jsoninfo);
+	$json = json_encode($jsoninfo);
+	$data->information = $json;
 	
 }
 
